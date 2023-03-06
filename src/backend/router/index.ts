aea18fadf951d0b5ed1b2e48d447472927b4e9db
Leisurellama <kameron000@yahoo.com>
2023-03-06 09:22:08 -0600

    resolve re-render issue and fetch

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,17 +1,69 @@
 import { z } from 'zod';
 import { procedure, router } from '../trpc';
+import * as trpc from '@trpc/server';
+
+import { PokemonClient } from 'pokenode-ts';
+
+const api = new PokemonClient();
+
 export const appRouter = router({
   hello: procedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
+    .input(z.object({ text: z.string() }))
     .query(({ input }) => {
       return {
-        greeting: `hello ${input.text}`,
+        greeting: `Hello ${input.text}`,
       };
     }),
+  // getAll: procedure.query(({ ctx }) => {
+  //   return ctx.prisma.example.findMany();
+  // }),
+  getPokemon: procedure.input(z.object({ id: z.number() }))
+    .query(async ({input}) => {
+    const data = await api.getPokemonById(input.id);
+    // console.log("called", input.id, data)
+    return data;
+  }),
 });
+
+// export const appRouter = router({
+//   getPokemonById: procedure
+//   .input(
+//     z.object({
+//       id: z.number(),
+//     }),
+//     )
+//     .query(({ input }) => {
+//       const api = new PokemonClient()
+      
+//       return {
+//         pokemon: api.getPokemonById(input.id)
+//       }
+      
+//     }),
+// });
+
+// export const appRouter = trpc.router().query('get-pokemon-by-id', {
+//   input: z.object({id: z.number()}),
+//   async resolve({input}) {
+//     const api = new PokemonClient();
+
+//     const pokemon = await api.getPokemonById(input.id);
+//     return pokemon;
+//   },
+// })
+
+  // router({
+  // hello: procedure
+  //   .input(
+  //     z.object({
+  //       text: z.string(),
+  //     }),
+  //   )
+  //   .query(({ input }) => {
+  //     return {
+  //       greeting: `hello ${input.text}`,
+  //     };
+  //   }),
+  // });
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Head from 'next/head'
 import { trpc } from '@/utils/trpc';
 import { getOptionsForVote } from '@/utils/getRandomPokemon';
+import { useState } from 'react';
 
-export default function Home() {
+export default function Home () {
 
-  const [first, second] = getOptionsForVote();
-  
+  const [ids, updateIds] = useState(getOptionsForVote());
+
+  const [first, second] = ids;
+
+  // console.log(first, second)
+
+  // const firstPokemon = trpc.useQuery("get-pokemon-by-id", { id: first });
+  const firstPokemon = trpc.getPokemon.useQuery({ id: first });
+  const secondPokemon = trpc.getPokemon.useQuery({ id: second });
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) {
+    return null;
+  }
+
   return (
     <>
       <Head>
@@ -19,9 +32,9 @@ export default function Home() {
           <div className='text-2xl text-center' >Which Pokemon is Rounder?</div>
           <div className='p-2' />
           <div className='border rounded p-8 flex justify-between items-center max-w-2xl' >
-            <div className='w-16 h-16 bg-red-800'>{first}</div>
+            <div className='w-64 h-64 bg-red-800'><img src={firstPokemon.data?.sprites.front_default} className="w-full"/></div>
             <div className='p-8'>VS</div>
-            <div className='w-16 h-16 bg-red-800'>{second}</div>
+            <div className='w-64 h-64 bg-red-800'><img src={secondPokemon.data?.sprites.front_default} className="w-full"/></div>
           </div>
 
         </div>
