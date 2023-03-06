6164a92bf618410ced1ab9db880375f99e080cf2
Leisurellama <kameron000@yahoo.com>
2023-03-06 09:33:02 -0600

    styling boxes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,15 @@ export default function Home () {
           <div className='text-2xl text-center' >Which Pokemon is Rounder?</div>
           <div className='p-2' />
           <div className='border rounded p-8 flex justify-between items-center max-w-2xl' >
-            <div className='w-64 h-64 bg-red-800'><img src={firstPokemon.data?.sprites.front_default} className="w-full"/></div>
+            <div className='w-64 h-64 flex flex-col'>
+              <img src={firstPokemon.data?.sprites.front_default} className="w-full"/>
+              <div className='text-xl text-center pb-4 capitalize mt-[-2rem]'>{firstPokemon.data?.name}</div>
+            </div>
             <div className='p-8'>VS</div>
-            <div className='w-64 h-64 bg-red-800'><img src={secondPokemon.data?.sprites.front_default} className="w-full"/></div>
+            <div className='w-64 h-64 flex flex-col'>
+              <img src={secondPokemon.data?.sprites.front_default} className="w-full"/>
+              <div className='text-xl text-center pb-4 capitalize mt-[-2rem]'>{secondPokemon.data?.name}</div>
+            </div>
           </div>
 
         </div>
