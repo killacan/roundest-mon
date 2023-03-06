408a0734fe72def0db15fdd2140b336262ff0080
Leisurellama <kameron000@yahoo.com>
2023-03-06 11:15:21 -0600

    add buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { trpc } from '@/utils/trpc';
 import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { useState } from 'react';
 
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 export default function Home () {
 
   const [ids, updateIds] = useState(getOptionsForVote());
@@ -19,6 +22,12 @@ export default function Home () {
     return null;
   }
 
+  const voteForRoundest = () => {
+    // todo: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  }
+
   return (
     <>
       <Head>
@@ -32,14 +41,16 @@ export default function Home () {
           <div className='text-2xl text-center' >Which Pokemon is Rounder?</div>
           <div className='p-2' />
           <div className='border rounded p-8 flex justify-between items-center max-w-2xl' >
-            <div className='w-64 h-64 flex flex-col'>
+            <div className='w-64 h-64 flex flex-col items-center'>
               <img src={firstPokemon.data?.sprites.front_default} className="w-full"/>
               <div className='text-xl text-center pb-4 capitalize mt-[-2rem]'>{firstPokemon.data?.name}</div>
+              <button className={btn} onClick={() => voteForRoundest(first)}>Rounder</button>
             </div>
             <div className='p-8'>VS</div>
-            <div className='w-64 h-64 flex flex-col'>
+            <div className='w-64 h-64 flex flex-col items-center'>
               <img src={secondPokemon.data?.sprites.front_default} className="w-full"/>
               <div className='text-xl text-center pb-4 capitalize mt-[-2rem]'>{secondPokemon.data?.name}</div>
+              <button className={btn} onClick={() => voteForRoundest(second)}>Rounder</button>
             </div>
           </div>
 
