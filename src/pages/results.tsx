5f8d26b24039418bcdf9a354c3351dd6ade605b3
Leisurellama <kameron000@yahoo.com>
2023-03-07 22:48:57 -0600

    add better results

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -30,16 +30,27 @@ const getPokemonInOrder = async () => {
 
 type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
 
-const PokemonListing: React.FC<{pokemon: PokemonQueryResult[number] }> = ( props ) => {
+const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
+    const {VoteFor, VoteAgainst} = pokemon._count;
+    if (VoteFor + VoteAgainst === 0) return 0;
+    return VoteFor / (VoteFor + VoteAgainst) * 100;  
+}
+
+const PokemonListing: React.FC<{pokemon: PokemonQueryResult[number] }> = ({ pokemon }) => {
     return (
-        <div className="flex border-b p-4 items-center">
-            <Image 
-                width={64} 
-                height={64} 
-                alt="" 
-                src={props.pokemon.spriteUrl!} 
-            />
-            <div className="capitalize" >{props.pokemon.name}</div>
+        <div className="flex border-b p-4 justify-between">
+            <div className="flex items-center" >
+                <Image 
+                    width={64} 
+                    height={64} 
+                    alt="" 
+                    src={pokemon.spriteUrl!} 
+                />
+                <div className="capitalize px-5" >{pokemon.name}</div>
+            </div>
+            <div className="flex items-center justify-end" >
+                {generateCountPercent(pokemon) + "%" }
+            </div>
         </div>
 
     )
